479b885d52db43926ca288303b6f09601b7ae591
IvanOrsh <ksironov@mail.ru>
2023-03-11 23:47:34 +0300

    add: initial setup for db
    - typeorm
    - pg
    - docker-compose
    - .env
    - ConfigModule, ConfigService wired

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { ConfigService } from '@nestjs/config';
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // TODO: remove below
+  @Get('config')
+  getConfig(): string {
+    const postgresUser = this.configService.get<string>('POSTGRES_USER');
+    const postgresPassword =
+      this.configService.get<string>('POSTGRES_PASSWORD');
+    const postgresDb = this.configService.get<string>('POSTGRES_DB');
+    const postgresUri = `postgresql://${postgresUser}:${postgresPassword}@postgres:5432/${postgresDb}`;
+    return `POSTGRES_URI is set to: ${postgresUri}`;
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'), // Specify the correct port number here
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
