63fb77af6eed051a3659a88c6265ea0b8769a108
IvanOrsh <ksironov@mail.ru>
2023-03-25 22:41:54 +0300

    feat: role-based auth

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -14,14 +14,18 @@ import { Observable } from 'rxjs';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { DeleteResult, UpdateResult } from 'typeorm';
 
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/models/role.enum';
 import { FeedPost } from '../models/post.interface';
 import { FeedService } from '../services/feed.service';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
-  @UseGuards(JwtGuard)
+  @Roles(Role.ADMIN, Role.PREMIUM)
+  @UseGuards(JwtGuard, RolesGuard)
   @Post()
   create(@Body() feedPost: FeedPost, @Request() req): Observable<FeedPost> {
     return this.feedService.createPost(req.user, feedPost);
